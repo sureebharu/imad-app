e6303ce87cbfcc6ed40f269839c237ea59479e9d
sureebharu <30650250+sureebharu@users.noreply.github.com>
2017-08-10 13:55:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/ui/madi.png', function (req, res) {
       res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
  });
  
-  app.get('/main.js' , function(req,res){
+  app.get('/ui/main.js' , function(req,res){
       res.sendFile(path.join(__dirname, 'ui', 'main.js'));
  });
  
